[Bugfix] fix precision issues for enforce-eager and cudagraph
Signed-off-by: gaojc <1055866782@qq.com>

vllm/v1/attention/backends/flashinfer.py
@@ -24,8 +24,9 @@ from vllm.attention.backends.abstract import (
     MultipleOf,
 )
 from vllm.attention.ops.common import cp_lse_ag_out_ar
+from vllm.attention.ops.merge_attn_states import merge_attn_states
 from vllm.config import CUDAGraphMode, VllmConfig
-from vllm.distributed.parallel_state import get_pcp_group, get_tp_group
+from vllm.distributed.parallel_state import get_pcp_group
 from vllm.logger import init_logger
 from vllm.model_executor.layers.batch_invariant import (
     vllm_is_batch_invariant,
@@ -429,7 +430,7 @@ class FlashInferMetadataBuilder(AttentionMetadataBuilder[FlashInferMetadata]):
 
     def _get_prefill_wrapper(self, attn_metadata):
         # if self._prefill_wrapper is None:
-        if self.pcp_world_size > 1 and attn_metadata.pcp_metadata is not None:
+        if self.pcp_world_size > 1:
             self._prefill_wrapper = {}
             for key in ["head_mask", "head_womask", "tail_mask", "tail_womask"]:
                 self._prefill_wrapper[key] = BatchPrefillWithRaggedKVCacheWrapper(
@@ -725,13 +726,13 @@ class FlashInferMetadataBuilder(AttentionMetadataBuilder[FlashInferMetadata]):
                 attn_metadata.max_q_len_prefill = int(query_lens_prefill.max().item())
 
                 if not attn_metadata.prefill_use_trtllm:
-                    if self.pcp_world_size > 1 and common_attn_metadata.pcp_metadata is not None:
+                    if self.pcp_world_size > 1:
                         assert common_attn_metadata.pcp_metadata is not None
                         assert common_attn_metadata.query_positions is not None
 
                         pcp_metadata = common_attn_metadata.pcp_metadata
                         qo_indptr_cpu = pcp_metadata.q_head_start_loc
-                        kv_mask_for_head_indptr = pcp_metadata.kv_nomask_for_head_indptr
+                        kv_mask_for_head_indptr = pcp_metadata.kv_mask_for_head_indptr
                         kv_nomask_for_head_indptr = pcp_metadata.kv_nomask_for_head_indptr
                         kv_mask_for_tail_indptr = pcp_metadata.kv_mask_for_tail_indptr
                         kv_nomask_for_tail_indptr = pcp_metadata.kv_nomask_for_tail_indptr
@@ -963,7 +964,7 @@ class FlashInferImpl(AttentionImpl):
         if self.sinks is not None and self.sinks.dtype != torch.float32:
             self.sinks = self.sinks.to(torch.float32)
 
-    def _attention_with_nomask_and_mask(self, q: torch.Tensor,
+    def attention_with_nomask_and_mask(self, q: torch.Tensor,
                                         k_mask: torch.Tensor,
                                         v_mask: torch.Tensor,
                                         k_nomask: torch.Tensor,
@@ -971,15 +972,8 @@ class FlashInferImpl(AttentionImpl):
                                         prefill_wrapper_mask: BatchPrefillWithRaggedKVCacheWrapper,
                                         prefill_wrapper_womask: BatchPrefillWithRaggedKVCacheWrapper):
         output_mask = torch.empty_like(q)
-        output_womask = torch.empty_like(q)
         lse_mask = torch.empty(
                         (q.size(0), q.size(1)),
-                        dtype=torch.float32,
-                        device=q.device,
-                    )
-        lse_womask = torch.empty(
-                        (q.size(0), q.size(1)),
-                        dtype=torch.float32,
                         device=q.device,
                     )
         prefill_wrapper_mask.run(q,
@@ -988,31 +982,32 @@ class FlashInferImpl(AttentionImpl):
                                  out=output_mask,
                                  lse=lse_mask,
                                  return_lse=True)
-        prefill_wrapper_womask.run(q,
-                                   k_nomask,
-                                   v_nomask,
-                                   out=output_womask,
-                                   lse=lse_womask,
-                                   return_lse=True)
-        output, _ = self._update_out_and_lse(
-                torch.stack([output_womask, output_mask], dim=0),
-                torch.stack([lse_womask.unsqueeze(-1), lse_mask.unsqueeze(-1)], dim=0))
+        if k_nomask.shape[0] != 0:
+            output_womask = torch.empty_like(q)
+            lse_womask = torch.empty(
+                        (q.size(0), q.size(1)),
+                        device=q.device,
+                    )
+            prefill_wrapper_womask.run(q,
+                                    k_nomask,
+                                    v_nomask,
+                                    out=output_womask,
+                                    lse=lse_womask,
+                                    return_lse=True)
+            
+            output = torch.empty_like(q)
+            output_lse = torch.empty_like(lse_womask)
+            merge_attn_states(
+                output=output,
+                output_lse=output_lse,
+                prefix_output=output_mask,
+                prefix_lse=lse_mask.transpose(0, 1).contiguous(),
+                suffix_output=output_womask,
+                suffix_lse=lse_womask.transpose(0, 1).contiguous(),
+            )
+        else:
+            output = output_mask
         return output
-    
-    def _update_out_and_lse(self, out_list: torch.Tensor,
-                            lse_list: torch.Tensor) -> torch.Tensor:
-        """LSE_final = log(sum(exp(LSE_i))), O_final = sum(exp(LSE_i - LSE_final) * O_i)
-        Args:
-            out_list: shape = [N, batch_size, num_heads, head_size]
-            lse_list: shape = [N, batch_size, num_heads, 1]
-        Returns:
-            out_final: shape = [batch_size, num_heads, head_size]
-            lse_final: shape = [batch_size, num_heads, 1]
-        """
-        lse_final = torch.logsumexp(lse_list, dim=0, keepdim=False)
-        out_final = torch.sum(torch.exp(lse_list - lse_final) * out_list,
-                              dim=0)
-        return out_final, lse_final
 
     def forward(
         self,
@@ -1176,7 +1171,8 @@ class FlashInferImpl(AttentionImpl):
             assert prefill_wrapper is not None
 
             if not attn_metadata.prefill_use_trtllm:
-                if self.pcp_world_size > 1 and attn_metadata.pcp_metadata is not None:
+                if self.pcp_world_size > 1:
+                    assert type(prefill_wrapper) ==  dict
                     for _, prefill_wrapper_i in prefill_wrapper.items():
                         assert prefill_wrapper_i._window_left == self.window_left
                         assert prefill_wrapper_i._logits_soft_cap == (self.logits_soft_cap or 0.0)
@@ -1189,12 +1185,13 @@ class FlashInferImpl(AttentionImpl):
                     kv_nomask_for_head_indices = pcp_metadata.kv_nomask_for_head_indices
                     kv_mask_for_tail_indices = pcp_metadata.kv_mask_for_tail_indices
                     kv_nomask_for_tail_indices = pcp_metadata.kv_nomask_for_tail_indices
+                    q_full_indices = pcp_metadata.q_full_indices
 
                     # NOTE(qcs): Allgather causes duplicate decoding tokens.
                     prefill_key = key[num_decode_tokens * self.pcp_world_size :]
                     prefill_value = value[num_decode_tokens * self.pcp_world_size :]
 
-                    output_head = self._attention_with_nomask_and_mask(
+                    output_head = self.attention_with_nomask_and_mask(
                         torch.index_select(prefill_query, 0, q_head_indices),
                         torch.index_select(prefill_key, 0, kv_mask_for_head_indices),
                         torch.index_select(prefill_value, 0, kv_mask_for_head_indices),
@@ -1204,7 +1201,7 @@ class FlashInferImpl(AttentionImpl):
                         prefill_wrapper["head_womask"],
                     )
 
-                    output_tail = self._attention_with_nomask_and_mask(
+                    output_tail = self.attention_with_nomask_and_mask(
                         torch.index_select(prefill_query, 0, q_tail_indices),
                         torch.index_select(prefill_key, 0, kv_mask_for_tail_indices),
                         torch.index_select(prefill_value, 0, kv_mask_for_tail_indices),
@@ -1214,16 +1211,12 @@ class FlashInferImpl(AttentionImpl):
                         prefill_wrapper["tail_womask"],
                     )
 
-                    q_full_idx = torch.cat([q_head_indices, q_tail_indices])
-                    q_full_idx = q_full_idx.to(torch.float32).argsort().to(torch.int32)
                     output_full = torch.index_select(
                         torch.cat([output_head, output_tail], dim=0),
                         0,
-                        q_full_idx
+                        q_full_indices
                     )
                     output[num_decode_tokens:] = output_full
-                    # if get_tp_group().rank_in_group == 0 and get_pcp_group().rank_in_group == 0:
-                    #     print(output_head.shape, output_head)
                 else:
                     assert prefill_wrapper._window_left == self.window_left
                     assert prefill_wrapper._logits_soft_cap == (self.logits_soft_cap or 0.0)

vllm/v1/worker/gpu_model_runner.py
@@ -1027,12 +1027,13 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             head_tail_indices[key] = torch.from_numpy(value).to(
                 device=self.device, dtype=torch.int64, non_blocking=True
             )
-        
         for key, value in head_tail_indptr.items():
             head_tail_indptr[key] = torch.from_numpy(value).to(
                 dtype=torch.int64
             )
-            # print(f"pcp_rank{self.pcp_rank}", key, head_tail_indptr[key])
+
+        q_full_indices = torch.cat([head_tail_indices["q_head"], head_tail_indices["q_tail"]])
+        q_full_indices = q_full_indices.to(torch.float32).argsort().to(torch.int32)
 
         return PrefillContextParallelMetadata(
             q_head_indices=head_tail_indices["q_head"],
@@ -1046,6 +1047,7 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             kv_mask_for_head_indptr=head_tail_indptr["kv_mask_head"],
             kv_nomask_for_tail_indptr=head_tail_indptr["kv_nomask_tail"],
             kv_mask_for_tail_indptr=head_tail_indptr["kv_mask_tail"],
+            q_full_indices=q_full_indices,
         )
         
     def _update_tokens_for_pcp(self, tokens):
@@ -2815,7 +2817,7 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
                 # NOTE we must `slice` hidden_states because pcp_allgather_restore_idx
                 # ignores the padding from CUDA Graph.
                 hidden_states = get_pcp_group().all_gather(
-                    hidden_states[:num_scheduled_tokens],
+                    hidden_states[:num_scheduled_tokens_np.sum()],
                     0,
                 )
                 hidden_states = torch.index_select(
@@ -3619,13 +3621,13 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             self.query_start_loc.np[1 : num_reqs + 1] = cum_num_tokens
             self.query_start_loc.copy_to_gpu()
             
-            # pcp_metadata = None
-            # if self.pcp_world_size > 1:
-            #     num_decode_reqs = sum(num_scheduled_tokens == 1)
-            #     pcp_metadata = self._get_pcp_metadata(
-            #         num_scheduled_tokens[num_decode_reqs:],
-            #         self.seq_lens.np[num_decode_reqs:num_reqs],
-            #     )
+            pcp_metadata = None
+            if self.pcp_world_size > 1:
+                num_decode_reqs = sum(num_scheduled_tokens == 1)
+                pcp_metadata = self._get_pcp_metadata(
+                    num_scheduled_tokens[num_decode_reqs:],
+                    num_scheduled_tokens[num_decode_reqs:] * 2,
+                )
             for kv_cache_group_id, kv_cache_group_spec in enumerate(
                 self.kv_cache_config.kv_cache_groups
             ):
@@ -3649,7 +3651,7 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
                     ].slot_mapping.gpu[:num_tokens],
                     causal=True,
                     query_positions=query_positions,
-                    pcp_metadata=None,
+                    pcp_metadata=pcp_metadata if self.pcp_world_size > 1 else None,
                     pcp_allgather_restore_idx=self.pcp_allgather_restore_idx.gpu[
                         : total_num_scheduled_tokens * self.pcp_world_size
                     ] if self.pcp_world_size > 1